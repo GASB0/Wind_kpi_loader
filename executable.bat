b4f10fb2c394d40b2a73c84d24e7679ce3771d88
Gabriel Santos <gsantos@wind.com.do>
2022-10-21 15:18:57 -0400

    Primera prueba funcional

---
+++ executable.bat
@@ -0,0 +1,4 @@
+call .\Scripts\activate.bat
+@echo off
+".\Scripts\python.exe" ".\script.py"
+pause
---
+++ script.py
@@ -0,0 +1,67 @@
+import sqlalchemy as sa
+import urllib
+import datetime
+import re
+import os
+import shutil
+import pandas as pd
+
+server='WIN-SNQUCAIM13C'
+database = 'KPI_4G'
+username= 'script_report'
+password = '49*pHdW*I4R2x'
+
+params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+print(params)
+
+engine = sa.create_engine("mssql+pyodbc:///?odbc_connect=%s"%params)
+engine.connect()
+
+
+def saveIntoDB(dataFrame, tableName, db_connection):
+    try:
+        dataFrame.to_sql(tableName, con=db_connection, if_exists='append', index=False)
+    except ValueError as vX:
+        print(vX)
+    except Exception as ex:
+        print(ex)
+    else:
+        print('The operation succeeded')
+
+# Lectura de los CSV:
+KPIsRoute='./test_kpis'
+KPIsBackUpRoute='./backup'
+
+for dirPath, dirNames, fileNames in os.walk(KPIsRoute):
+    for fileName in fileNames:
+        fileRoute = os.path.join(dirPath, fileName)
+        KPIDataFrame = pd.read_csv(fileRoute).drop(columns=['Index'])
+
+        for column in KPIDataFrame.columns:
+            newColumnName = re.sub(r'\(.*\)','', column.replace(' ', '_')).strip()
+            KPIDataFrame.rename(columns={column:newColumnName}, inplace=True)
+
+        KPIDataFrame['End_Time'] = pd.to_datetime(KPIDataFrame['End_Time'], infer_datetime_format=True)
+        KPIDataFrame['Start_Time'] = pd.to_datetime(KPIDataFrame['Start_Time'], infer_datetime_format=True)
+
+        # print(KPIDataFrame.head())
+
+        # shutil.move(fileRoute, KPIsBackUpRoute)
+
+        if 'RAN' in fileName and (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d") in fileName:
+            print(fileName)
+            saveIntoDB(KPIDataFrame, 'ran_kpis', engine)
+
+
+# Cierre de conexion
+engine.dispose()
+# conn = pyodbc.connect('Driver={SQL Server};'
+#                      'Server=WIN-SNQUCAIM13C;'
+#                      'Database=KPI_WAREHOUSE;'
+#                      'Trusted_Connection=yes;')
+
+# cursor = conn.cursor()
+# cursor.execute('SELECT * FROM Test')
+
+# for i in cursor:
+#    print(i)
